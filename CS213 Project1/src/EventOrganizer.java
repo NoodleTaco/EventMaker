909bca6da3cf7d1a52c634da901e1b3e89beade9
Donald Yubeaton <nasusneverna@gmail.com>
2023-09-24 15:18:22 -0400

    Implemented EventOrganizer Testbed Main with scanner methodology.

--- CS213 Project1/src/EventOrganizer.java
+++ CS213 Project1/src/EventOrganizer.java
@@ -1,2 +1,32 @@
-public class EventOrganizer {
+import java.util.Scanner;
+import java.util.StringTokenizer;
+public class EventOrganizer
+{
+    public void run()
+    {
+
+    }
+    //Testbed main, testing a loop that can handle multiple command line inputs and figuring out how string tokenizer sorts data
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Input:");
+        while(true)
+        {
+            String line = scanner.nextLine();
+            if (line.equals("Q"))
+            {
+                System.out.println("Closing EventOrganizer");
+                break; // Exit the loop
+            }
+            StringTokenizer st = new StringTokenizer(line);
+            while (st.hasMoreTokens())
+            {
+                System.out.println(st.nextToken());
+            }
+
+        }
+
+    }
+
 }
