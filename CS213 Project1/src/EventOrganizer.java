c34c76e7ba9866d1c6f39cc73694395a86ee5631
Donald Yubeaton <nasusneverna@gmail.com>
2023-09-24 23:08:55 -0400

    Implemented main methodology of EventOrganizer and RunProject1

--- CS213 Project1/src/EventOrganizer.java
+++ CS213 Project1/src/EventOrganizer.java
@@ -2,11 +2,71 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 public class EventOrganizer
 {
+    private EventCalendar eventCalendar;
+
+    public EventOrganizer()
+    {
+        eventCalendar = new EventCalendar();
+    }
+
     public void run()
     {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Event Organizer running....");
+        while(true)
+        {
+            String line = scanner.nextLine();
+            if (line.equals("Q"))
+            {
+                System.out.println("Event Organizer terminated");
+                break; // Exit the loop
+            }
+            StringTokenizer stringTokenizer = new StringTokenizer(line);
+            processInput(stringTokenizer);
+        }
 
     }
-    //Testbed main, testing a loop that can handle multiple command line inputs and figuring out how string tokenizer sorts data
+
+    private void processInput(StringTokenizer stringTokenizer)
+    {
+        if(stringTokenizer.hasMoreTokens())
+        {
+            String command = stringTokenizer.nextToken();
+            if(command.equals("A"))
+            {
+
+            }
+            else if(command.equals("R"))
+            {
+
+            }
+            else if(command.equals("P"))
+            {
+                eventCalendar.print();
+            }
+            else if(command.equals("PE"))
+            {
+                eventCalendar.printByDate();
+            }
+            else if(command.equals("PC"))
+            {
+                eventCalendar.printByCampus();
+            }
+            else if(command.equals("PD"))
+            {
+                eventCalendar.printByDepartment();
+            }
+        }
+    }
+    //Using temporarily for testing
+    private static void printTokenizer(StringTokenizer stringTokenizer)
+    {
+        while (stringTokenizer.hasMoreTokens())
+        {
+            System.out.println(stringTokenizer.nextToken());
+        }
+    }
+    //Temporary Testbed Main
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
@@ -20,10 +80,7 @@ public class EventOrganizer
                 break; // Exit the loop
             }
             StringTokenizer st = new StringTokenizer(line);
-            while (st.hasMoreTokens())
-            {
-                System.out.println(st.nextToken());
-            }
+
 
         }
 
